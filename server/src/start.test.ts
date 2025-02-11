eeb53855025c80f48fa4b91021af551dddd1710e
Annelie <anneliehanssons@gmail.com>
2025-02-11 21:57:44 +0100

    test: add E2E test

--- server/src/start.test.ts
+++ server/src/start.test.ts
@@ -7,8 +7,10 @@ import { Expense } from "./model/expense.interface";
 const request = SuperTest.default(app);
 
 test("End-to-end test", async() => {
-    const category= "Rent"
-    const cost = 4000
+    const category= "Rent";
+    const cost = 4000;
+    const expenseCost = 3000;
+    const desc = "house"
 
     //Creating budget
     const res1 = await request.post("/budget").send({category, cost});
@@ -16,11 +18,18 @@ test("End-to-end test", async() => {
     expect(res1.body.category).toEqual(category);
     expect(res1.body.cost).toEqual(cost);
 
-    //Verify budget
-    const res2 = await request.get("/budget");
-    expect(res2.statusCode).toEqual(200);
-    expect(res2.body.some((budget: Budget) => budget.category === category && budget.cost === cost)).toBeTruthy();
+    //Create expense
+    const res2 = await request.post("/expense").send({ category, cost: expenseCost, description: desc});
+    expect(res2.statusCode).toEqual(201);
+    expect(res2.body.category).toEqual(category);
+    expect(res2.body.cost).toEqual(expenseCost);
+    expect(res2.body.description).toEqual(desc);
 
-    
+    const expenseId = res2.body.id;
+
+    //Verify budget 
+    const rest3 = await request.get("/budget");
+    expect(rest3.statusCode).toEqual(200);
+    expect(rest3.body.some((budget: Budget) => budget.category === category && budget.result === (cost - expenseCost))).toBeTruthy();
 
 })
