cc2beab0b86b5da65174877fb4b91c4ca290e752
Liam <liamn.mayor@gmail.com>
2025-02-12 10:58:09 +0100

    Close #19. Setup of react in client and cors in server.

--- server/src/model/budget.interface.ts
+++ server/src/model/budget.interface.ts
@@ -1,9 +1,10 @@
 import { Expense } from "./expense.interface";
 
+// Invariant: result === cost - sum of expenses
 export interface Budget {
-    category : string;
-    cost : number;
-    expenses : Expense[];
-    result : number;
+    category: string;
+    cost: number;
+    expenses: Expense[];
+    result: number;
 }
 
--- server/src/start.ts
+++ server/src/start.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { expenseRouter } from "./router/expense";
 import { budgetRouter } from "./router/budget";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/expense", expenseRouter);
 app.use("/budget", budgetRouter);
