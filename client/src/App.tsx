f9168a8bd4935d2862824dde98549d289f26e519
Lee4-M <liamn.mayor@gmail.com>
2025-02-17 19:27:30 +0100

    Connect budget frontend to backend. Add modal panel for budget and corresponding button in sidebar. Ref #21. Ref #17
    Co-authored-by: Kevin Collins <Kev049@users.noreply.github.com>
    Co-authored-by: IntePhilip <IntePhilip@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,36 @@
 import './App.css'
-
+import { Budget, getBudgets } from './api';
 import { Sidebar } from './Sidebar'
 import { HelpSettings } from './HelpSettings'
 import { BudgetTable } from './BudgetTable'
 
+import { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [budgets, setBudgets] = useState<Budget[]>([]);
+
+  async function loadBudgets() {
+    const budgets = await getBudgets();
+    setBudgets(budgets);
+  }
+  
+  function addBudget(newBudget: Budget) {
+    setBudgets((prevBudgets) => [...prevBudgets, newBudget]);
+  }
+  
+  // Som onMount i Svelte, körs när komponenten renderas.
+  // Inte helt säker om detta funkar som tänkt
+  useEffect(() => {
+      loadBudgets();
+  }, [loadBudgets]);
+
   return (
     <Container fluid className="bg-body-secondary h-100 w-100">
       <Row className='h-100'>
-        <Col lg="2" className='p-3'><Sidebar /></Col>
-        <Col lg="9" className='p-3'><BudgetTable /></Col>
+        <Col lg="2" className='p-3'><Sidebar addBudget={addBudget} loadBudgets={loadBudgets}  /></Col>
+        <Col lg="9" className='p-3'><BudgetTable budgets={budgets}/></Col>
         <Col lg="1" className='p-0'><HelpSettings /></Col>
       </Row>
     </Container>
---
+++ client/src/BudgetItemModal.tsx
@@ -0,0 +1,59 @@
+import { Modal, Button, Form } from 'react-bootstrap';
+import { addBudget, Budget } from "./api";
+import { useState } from 'react';
+
+
+interface BudgetModalProps {
+    show: boolean;
+    handleClose: () => void;
+    onSave: (budget: Budget) => void;
+}
+
+function BudgetItemModal({ show, handleClose, onSave }: BudgetModalProps) {
+    
+    const [category, setCategory] = useState<string>('');
+    const [amount, setAmount] = useState<number | ''>('');
+
+    async function saveBudgetItems() {
+        const newBudgetItem = await addBudget(category, Number(amount));
+        if (newBudgetItem) {
+            // Måste se till att formuläret töms efter vi sparat
+            onSave(newBudgetItem);
+            handleClose();
+            setCategory('');
+            setAmount('');
+        } else {
+            alert('Failed to add budget');
+        }
+    }
+
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add Budget Item</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Budget Category</Form.Label>
+                        <Form.Control type="text" placeholder="Enter category name" value={category} 
+                        onChange={(e) => setCategory(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Amount</Form.Label>
+                        <Form.Control type="number" placeholder="Enter amount" value={amount} 
+                        onChange={(e) => setAmount(e.target.value === '' ? '' : Number(e.target.value))}/>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={() => { handleClose() }}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={saveBudgetItems}>Save Budget</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+export default BudgetItemModal;
