893d4cd1a1091f7f69f9d79f088fd1e562b62907
IntePhilip <phille97000@gmail.com>
2025-03-03 15:57:20 +0100

    small fix

--- server/src/service/IExpenseService.ts
+++ server/src/service/IExpenseService.ts
@@ -10,5 +10,5 @@ export interface IExpenseService {
     // Marks the task with the given id number as done, and returns true
     // Returns false if there is no task with that id number
     // If the task is already done, this method performs no operation and returns true
-    deleteExpense(username: String, id: String): Promise<void>
+    removeExpense(username: String, id: String): Promise<void>
   }
