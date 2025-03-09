e2e45830267889a194b5865ebb1f830b9657a108
Lee4-M <liamn.mayor@gmail.com>
2025-03-09 22:51:03 +0100

    Change conn.ts to use Aiven online database. Add ca.pem to gitignore. Closes #50

--- server/src/db/conn.ts
+++ server/src/db/conn.ts
@@ -1,6 +1,31 @@
+import { readFileSync } from 'fs';
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-export const sequelize = new Sequelize('postgres://postgres@localhost:5432/postgres');
+dotenv.config();
+
+const DB_NAME = process.env.DB_NAME as string;
+const DB_USER = process.env.DB_USER as string;
+const DB_PASSWORD = process.env.DB_PASSWORD as string;
+const DB_HOST = process.env.DB_HOST as string;
+const DB_SSL_CA_PATH = process.env.DB_SSL_CA as string;
+const DB_PORT = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432; 
+
+const cCa = readFileSync(DB_SSL_CA_PATH, 'utf-8');
+
+export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+        host: DB_HOST,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: true,
+                ca: cCa
+            }
+        },
+        port: DB_PORT
+    }
+);
 
 export async function initDB() {
     try {
