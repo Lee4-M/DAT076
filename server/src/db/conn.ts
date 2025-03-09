001c5c153f18020a8184399f335427fb854bbe4f
Lee4-M <liamn.mayor@gmail.com>
2025-03-09 23:55:28 +0100

    Fix lint errors. Closes Online Database #50

--- server/src/db/conn.ts
+++ server/src/db/conn.ts
@@ -4,12 +4,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const DB_NAME = process.env.DB_NAME as string;
-const DB_USER = process.env.DB_USER as string;
-const DB_PASSWORD = process.env.DB_PASSWORD as string;
-const DB_HOST = process.env.DB_HOST as string;
-const DB_SSL_CA_PATH = process.env.DB_SSL_CA as string;
-const DB_PORT = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432; 
+const DB_NAME: string = process.env.DB_NAME!
+const DB_USER: string = process.env.DB_USER!
+const DB_PASSWORD:string = process.env.DB_PASSWORD!
+const DB_HOST:string = process.env.DB_HOST!
+const DB_SSL_CA_PATH:string = process.env.DB_SSL_CA!
+const DB_PORT: number = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432; 
 
 const cCa = readFileSync(DB_SSL_CA_PATH, 'utf-8');
 
